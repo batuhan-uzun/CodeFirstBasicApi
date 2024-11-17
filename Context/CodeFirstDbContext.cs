4bc133a200de390398a65627d75561e4bfcab53f
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-11-17 12:22:55 +0300

    Add project files.

---
+++ Context/CodeFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeFirstBasicApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasicApi.Context
+{
+    public class CodeFirstDbContext : DbContext
+    {
+        public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<GameEntity> Games => Set<GameEntity>();
+        DbSet<MovieEntity> Movies => Set<MovieEntity>();
+    }
+}
